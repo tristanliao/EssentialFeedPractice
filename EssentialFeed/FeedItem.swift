22c35775d826ec0a60d919d2554d0e6ff844c352
TristanLiao <tristan@17.media>
2022-01-15 20:33:11 +0800

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Bang Chiang Liao on 2022/1/15.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
